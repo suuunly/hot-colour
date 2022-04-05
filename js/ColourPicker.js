5337bb8bd657c826945c7c46cdf51c1502ade64d
suuunly <suuunly@gmail.com>
2022-04-05 21:19:12 +0000

    Deploying to gh-pages from  @ d2e6184a23f69bdda1a56b17e31d7dfddc758228 🚀

---
+++ js/ColourPicker.js
@@ -0,0 +1,29 @@
+﻿var colorPicker;
+
+export function InitializeColourPicker(colourSelectedCallback) {
+    colorPicker = new iro.ColorPicker("#picker", {
+        layout: [{
+            component: iro.ui.Wheel,
+            options: {}
+        }],
+        width: 500
+    });
+
+    colorPicker.on("input:change", (colorData) => {
+        const hue = colorData["$"];
+        if (!hue) {
+            return;
+        }
+
+        const h = hue.h || 0;
+        const s = hue.s || 100;
+        const l = hue.l || 50;
+
+        colourSelectedCallback.invokeMethodAsync("OnSelectedColour", h, s, l);
+    });
+}
+
+// TODO: select the colour when a player selects a colour
+export function SetColour(h, s, l) {
+    colorPicker.color.hsl = {h, s, l};
+}
